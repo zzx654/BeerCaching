a0e8ed9f4215b579f465cbfa0f511cac08ea8950
LeeMyoungHan <mhyhl654@gmail.com>
2023-06-06 17:29:19 +0900

    Rest Api,Local DB 정의

---
+++ data/src/main/java/com/example/data/mappers/BeerMappers.kt
@@ -0,0 +1,28 @@
+package com.example.data.mappers
+
+import com.example.data.local.BeerEntity
+import com.example.data.remote.BeerDto
+import com.example.domain.Beer
+
+fun BeerDto.toBeerEntity(): BeerEntity {
+    return BeerEntity(
+        id = id,
+        name = name,
+        tagline = tagline,
+        description = description,
+        firstBrewed = first_brewed,
+        imageUrl = image_url
+    )
+}
+
+fun BeerEntity.toBeer(): Beer {
+    return Beer(
+        id = id,
+        name = name,
+        tagline = tagline,
+        description = description,
+        firstBrewed = firstBrewed,
+        imageUrl = imageUrl
+    )
+
+}
